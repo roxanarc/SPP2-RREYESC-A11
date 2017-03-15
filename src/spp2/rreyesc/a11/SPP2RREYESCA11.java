7fa72a4f30150e1084591c050f5022f9079033b5
roxana <roxana@192.168.1.74>
2017-03-14 22:58:05 -0600

    Actividad

---
+++ src/spp2/rreyesc/a11/SPP2RREYESCA11.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp2.rreyesc.a11;
+
+/**
+ *
+ * @author roxana
+ */
+public class SPP2RREYESCA11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        ventana2 window2 = new ventana2();
+        window2.show();
+        
+        ventana3 window3 = new ventana3();
+        window3.show();
+    }
+    
+}
+    
+    
+
